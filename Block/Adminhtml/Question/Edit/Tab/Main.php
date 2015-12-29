57460dd00e74e31a079433c21f4981bccd6ccff7
Vladan Kuzmanovic <vkuzmanovic@crimsonwing.com>
2015-12-29 14:20:24 +0100

    validation fix

--- Block/Adminhtml/Question/Edit/Tab/Main.php
+++ Block/Adminhtml/Question/Edit/Tab/Main.php
@@ -101,7 +101,6 @@ class Main extends \Magento\Backend\Block\Widget\Form\Generic implements \Magent
                 'name' => 'product_id',
                 'label' => __('Product Id'),
                 'title' => __('Product Id'),
-                'class' =>'validate-number',
 				'note' => __('Comma separated Product Ids')
 			]
         );
